a5f0849df0359146246702a42aa4a981e0b3843f
Tetiana Kravchuk <tetiana.kravchuk1608@gmail.com>
2017-03-28 20:49:03 -0400

    Creating a contact and ContactData

---
+++ src/test/java/ru/stqa/ptf/sandbox/ContactCreationTest.java
@@ -0,0 +1,94 @@
+package ru.stqa.ptf.sandbox;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ContactCreationTest {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        login();
+    }
+
+    private void login() {
+        wd.get("http://localhost/addressbook/");
+        wd.findElement(By.id("header")).click();
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.xpath("//form[@id='LoginForm']//label[.='Password:']")).click();
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void ContactCreationTest() {
+
+        initContactCreation();
+        fillOutContactForm(new ContactData("Tetiana", "Kravchuk", "Infomatrix Global", "9789685469", "teti.kravchuk@gmail.com"));
+        submitContactCreation();
+        returnHomePage();
+        logOut();
+    }
+
+    private void logOut() {
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+    private void returnHomePage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    private void submitContactCreation() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillOutContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+        wd.findElement(By.name("theform")).click();
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    }
+
+    private void initContactCreation() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+
+    public static boolean isAlertPresent(ChromeDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
